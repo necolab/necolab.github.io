4ab64e865993349743e06668634514bf48f4db3d
Shota Shimazu <hornet.live.mf@gmail.com>
2019-04-06 12:54:23 +0900

    remove header shadow

--- src/App.js
+++ src/App.js
@@ -1,16 +1,67 @@
 import React, { Component } from 'react';
+import styled from 'styled-components';
 import { BigHeader } from './components/BigHeader';
 import { NavigationBar } from './components/NavBar';
-import { Button } from 'react-bootstrap';
+import { Container, Row, Col } from 'react-bootstrap';
 import './App.scss';
 
+
+const Heading = styled.h1`
+    font-size: 7rem;
+    line-height: 11rem;
+    word-break : normal;
+    font-weight: bold;
+    letter-spacing: 5px;
+    font-family: 'Noto Sans JP', sans-serif;
+    margin-top: 50px;
+`;
+
+const Line = styled.span`
+    padding-left: ${props => props.padding};
+`;
+
+const Highlight = styled.span`
+    color: white;
+    background: #3b3c3f;
+`;
+
 class App extends Component {
+
+    componentDidMount() {
+        var hostName = document.location.hostname;
+        if  ( hostName === "localhost" || hostName === "127.0.0.1" ) {
+            console.log("THIS IS DEBUG!");
+        } else {
+            alert("ただいま、このページは更新中です。お手数おかけして申し訳ございません。");
+            alert("NECOの履修に関しては4/11のRG合同説明会へ起こしください。");
+        }
+    }
+
     render() {
         return (
             <>
             <NavigationBar/>
             <BigHeader/>
-            <h1>This web site is now under construction.</h1>
+
+            <>
+                <Container>
+                    <Row>
+                        <Col>
+                            <Heading>
+                                <Line padding="20px"><Highlight>情報技術</Highlight>を用いて<br/></Line>
+                                <Line padding="150px">社会の<br/></Line>
+                                <Line padding="20px"><Highlight>問題</Highlight>の<Highlight>解決策</Highlight>を<br/></Line>
+                                <Line padding="320px">実践していく</Line>
+                            </Heading>
+                        </Col>
+                    </Row>
+                    <Row>
+                        <Col>1 of 3</Col>
+                        <Col>2 of 3</Col>
+                        <Col>3 of 3</Col>
+                    </Row>
+                </Container>
+            </>
             </>
         );
     }
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -26,7 +26,7 @@ export class NavigationBar extends Component {
             <Navbar.Collapse id="basic-navbar-nav">
                 <Nav className="mr-auto">
                     <Nav.Link href="#home">Home</Nav.Link>
-                    <Nav.Link href="#link">Link</Nav.Link>
+                    <Nav.Link href="#link">Projects</Nav.Link>
                     <NavDropdown title="Dropdown" id="basic-nav-dropdown">
                         <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
                         <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
