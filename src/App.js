f35a34e91f18f194f4f5c0fe9d6bb7c6a3174140
Shota Shimazu <hornet.live.mf@gmail.com>
2019-04-10 21:21:46 +0900

    revise

--- src/App.js
+++ src/App.js
@@ -1,10 +1,33 @@
 import React, { Component } from 'react';
+import styled from 'styled-components';
 import { BigHeader } from './components/BigHeader';
 import { NavigationBar } from './components/NavBar';
 import { Container, Row, Col } from 'react-bootstrap';
 import './App.scss';
 
+
+const Heading = styled.h1`
+    font-size: 7rem;
+    font-weight: bold;
+    font-family: 'Noto Sans JP', sans-serif;
+`;
+
+const Highlight = styled.span`
+    color: red;
+`;
+
 class App extends Component {
+
+    componentDidMount() {
+        var hostName = document.location.hostname;
+        if  ( hostName == "localhost" || hostName == "127.0.0.1" ) {
+            console.log("THIS IS DEBUG!");
+        } else {
+            alert("ただいま、このページは更新中です。お手数おかけして申し訳ございません。");
+            alert("NECOの履修に関しては4/11のRG合同説明会へ起こしください。");
+        }
+    }
+
     render() {
         return (
             <>
@@ -15,12 +38,10 @@ class App extends Component {
                 <Container>
                     <Row>
                         <Col>
-                            <h1>About us</h1>
-                        </Col>
-                    </Row>
-                    <Row>
-                        <Col>
-                            <h1>IoTでビッグデータをAIしてBlockchain</h1>
+                            <Heading>
+                                <Highlight>情報技術</Highlight>
+                                を用いて社会の中にある<Highlight>問題</Highlight>に対する<Highlight>解決策</Highlight>を実践していく
+                            </Heading>
                         </Col>
                     </Row>
                     <Row>
