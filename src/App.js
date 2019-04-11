2a05b63022218874de056ca12e4e32bbf7302983
Shota Shimazu <hornet.live.mf@gmail.com>
2019-04-11 15:13:27 +0900

    add neco logo

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
 import styled from 'styled-components';
-import { BigHeader } from './components/BigHeader';
+import { Card } from './components/Cards';
 import { NavigationBar } from './components/NavBar';
 import { Container, Row, Col } from 'react-bootstrap';
 import './App.scss';
+import logo from './assets/necolab.png';
 
 
-const Heading = styled.h1`
+const MegaHeading = styled.h1`
     font-size: 7rem;
     line-height: 11rem;
     word-break : normal;
@@ -14,6 +15,11 @@ const Heading = styled.h1`
     letter-spacing: 5px;
     font-family: 'Noto Sans JP', sans-serif;
     margin-top: 50px;
+
+    @media screen and (max-width: 800px) {
+        font-size: 4rem;
+        line-height: 6rem;
+    }
 `;
 
 const Line = styled.span`
@@ -23,7 +29,40 @@ const Line = styled.span`
 const Highlight = styled.span`
     color: white;
     background: #3b3c3f;
+    transition: all .3s;
+
+    &:hover {
+        background: #206be5;
+        font-size: 7.5rem;
+    }
+
+    @media screen and (max-width: 800px) {
+        &:hover {
+            font-size: 4.5rem;
+        }
+    }
 `;
+
+
+const Heading = styled.h1`
+    font-size: 5rem;
+    font-family: 'Roboto Condensed', sans-serif;
+    font-weight: bold;
+    margin-top: 2rem;
+
+    @media screen and (max-width: 800px) {
+        font-size: 2.5rem;
+    }
+`;
+
+const LogoImg = styled.img`
+    width: 80%;
+    margin: 0 auto;
+    margin: 8rem;
+    margin-top: 5rem;
+`;
+
+
 
 class App extends Component {
 
@@ -41,27 +80,56 @@ class App extends Component {
         return (
             <>
             <NavigationBar/>
-            <BigHeader/>
-
-            <>
+            <LogoImg src={logo}/>
+        
                 <Container>
                     <Row>
                         <Col>
-                            <Heading>
+                            <MegaHeading>
                                 <Line padding="20px"><Highlight>情報技術</Highlight>を用いて<br/></Line>
                                 <Line padding="150px">社会の<br/></Line>
                                 <Line padding="20px"><Highlight>問題</Highlight>の<Highlight>解決策</Highlight>を<br/></Line>
                                 <Line padding="320px">実践していく</Line>
-                            </Heading>
+                            </MegaHeading>
+                        </Col>
+                    </Row>
+                    <Row>
+                        <Col>
+                            <Heading>PROJECTS</Heading>
+                        </Col>
+                    </Row>
+                    <Row>
+                        <Col>
+                            <Card up><h1>AAA</h1></Card>
+                        </Col>
+                        <Col>
+                            <Card down><h1>AAA</h1></Card>
+                        </Col>
+                    </Row>
+                    <Row>
+                        <Col>
+                            <Heading>MEMBERS</Heading>
+                        </Col>
+                    </Row>
+                    <Row>
+                        <Col>
+                            <Card><h1>AAA</h1></Card>
+                        </Col>
+                        <Col>
+                            <Card><h1>AAA</h1></Card>
+                        </Col>
+                    </Row>
+
+                    <Row>
+                        <Col>
+                            <Heading>CONTACT</Heading>
                         </Col>
                     </Row>
                     <Row>
                         <Col>1 of 3</Col>
                         <Col>2 of 3</Col>
-                        <Col>3 of 3</Col>
                     </Row>
                 </Container>
-            </>
             </>
         );
     }
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -25,7 +25,7 @@ export class NavigationBar extends Component {
             <Navbar.Toggle aria-controls="basic-navbar-nav" />
             <Navbar.Collapse id="basic-navbar-nav">
                 <Nav className="mr-auto">
-                    <Nav.Link href="#home">Home</Nav.Link>
+                    <Nav.Link href="/">Home</Nav.Link>
                     <Nav.Link href="#link">Projects</Nav.Link>
                     <NavDropdown title="Dropdown" id="basic-nav-dropdown">
                         <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
