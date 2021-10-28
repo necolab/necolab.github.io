aa2a6510173cd86a68856cffd7dd5803136b3e2b
Shota Shimazu <shotastage@users.noreply.github.com>
2021-10-28 18:49:25 +0900

    add initial component

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
 import React from 'react';
+import { Navbar, NavBrand } from './components/Navbar';
 import './App.css';
 
 const App = () => {
   return (
     <React.Fragment>
+      <Navbar>
+        <NavBrand>NECO Lab</NavBrand>
+      </Navbar>
       <p>Hello, world</p>
     </React.Fragment>
   );
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,4 +1,30 @@
 import styled from 'styled-components';
 
 export const Navbar = styled.nav`
+  z-index: 100;
+  display: flex;
+  align-items: center;
+  align-content: center;
+  justify-content: space-between;
+  background: white;
+  filter: drop-shadow(0px 0px 1px rgb(184, 184, 184));
+  width: 100%;
+  min-height: 60px;
+  margin: 0;
+  top: 0;
+  @media screen and (max-width: 480px) {
+    height: 60px;
+  }
+`;
+
+export const NavBrand = styled.h1`
+  font-family: "Lora", serif;
+  font-size: 1.5rem;
+  margin: 0;
+  padding-left: 25px;
+  @media screen and (max-width: 480px) {
+    height: 60px;
+    line-height: 60px;
+    padding-left: 15px;
+  }
 `;
