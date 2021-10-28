b6fbc00fe211ec9b9d8ed0e7052dfdc804ea8bf7
Shota Shimazu <shotastage@users.noreply.github.com>
2021-10-28 18:53:10 +0900

    add router

---
+++ src/routers/main-router.tsx
@@ -0,0 +1,20 @@
+import {
+    BrowserRouter as Switch,
+    Route,
+    BrowserRouter,
+  } from "react-router-dom";
+
+  // Import pages
+  import App from "../App";
+
+  const Router = () => {
+    return (
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/" component={App} />
+        </Switch>
+      </BrowserRouter>
+    );
+  }
+
+  export default Router;
